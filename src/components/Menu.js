211c73bd855c7b44dd2f99091f63934797627400
Ilari Tuimala <ijtuimala@gmail.com>
2024-08-29 14:09:57 +0300

    More changes

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import recipes from '../recipes';
+
+const Menu = () => {
+    return (
+        <div className='menu-container'>
+            <div className='menu-header'>
+                <h2>Our Specials!</h2>
+                <button>Online Menu</button>
+            </div>
+            <div className='cards'>
+                {
+                    recipes.map(recipe => <div key={recipe.id} className='menu-items'>
+                        <img src={recipe.image} alt=''/>
+                        <div className='menu-content'>
+                            <div className='heading'>
+                                <h5>{recipe.title}</h5>
+                                <p>{recipe.price}</p>
+                            </div>
+                            <p>{recipe.description}</p>
+                            <button className='orderBtn'>Order a delivery</button>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Menu;
